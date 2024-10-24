2e1399eea490c5419cd8ae26b15e17ce24b2f4f2
kartik <whonalekartik11@gmail.com>
2024-10-24 11:21:26 +0530

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+
+let secretNumber = Math.floor(Math.random() * 1000) + 1;
+let attempts = 10;
+let currentAttempt = 0;
+const flag = FLAG1;
+
+app.post('/guess', (req, res) => {
+    const guess = req.body.guess;
+
+    if (typeof guess !== 'number') {
+        return res.status(400).json({ error: "Please provide a valid number." });
+    }
+
+    currentAttempt++;
+
+    if (currentAttempt > attempts) {
+        return res.json({ response: "Sorry, you've used all your attempts." });
+    }
+
+    if (guess < secretNumber) {
+        return res.json({ response: "Your guess is too low!" });
+    } else if (guess > secretNumber) {
+        return res.json({ response: "Your guess is too high!" });
+    } else {
+        return res.json({ response: `Correct! The flag is: ${flag}` });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Guess the Number app listening at http://localhost:${port}`);
+});
